34a5b1f4618db9a461868595d3160cdf47df9e99
Raad05 <ashabulraad@gmail.com>
2023-10-15 00:31:55 +0600

    Bug fixes

--- contracts/RecipeContract.sol
+++ contracts/RecipeContract.sol
@@ -2,43 +2,37 @@
 pragma solidity ^0.8.9;
 
 contract RecipeContract {
-    struct Reward {
-        uint256 fundAmount;
-        string rewardDescription;
-    }
-
     struct Recipe {
         string name;
         string[] ingredients;
         uint256 fundAmount;
         address recipeOwnerAddress;
         uint256 fundRaisedSoFar;
-        uint256 timeForFund;
-        uint256[] rewardIndexes; // Store the indexes of rewards in the rewards array
+        string timeForFund;
     }
+    mapping(string => uint256) private recipeIdx;
 
-    Recipe[] public recipes;
-    Reward[] public rewards;
-    mapping(string => uint256) public recipeNameToIndex;
+    Recipe[] private recipes;
 
     function createRecipe(
         string memory _name,
         string[] memory _ingredients,
         uint256 _fundAmount,
-        uint256 _timeForFund
+        string memory _timeForFund
     ) public {
-        recipeNameToIndex[_name] = recipes.length;
+        recipeIdx[_name] = recipes.length;
         Recipe memory newRecipe;
         newRecipe.name = _name;
         newRecipe.ingredients = _ingredients;
         newRecipe.fundAmount = _fundAmount;
         newRecipe.timeForFund = _timeForFund;
         newRecipe.recipeOwnerAddress = msg.sender;
+        newRecipe.fundRaisedSoFar = 0;
         recipes.push(newRecipe);
     }
 
     function getRecipe(
-        uint256 recipeIndex
+        string memory _name
     )
         public
         view
@@ -46,65 +40,46 @@ contract RecipeContract {
             string memory name,
             string[] memory ingredients,
             uint256 fundAmount,
-            uint256 timeForFund,
-            uint256[] memory rewardIndexes
+            address recipeOwnerAddress,
+            uint256 fundRaisedSoFar,
+            string memory timeForFund
         )
     {
+        uint256 recipeIndex = recipeIdx[_name];
         require(recipeIndex < recipes.length, "Recipe does not exist");
-        require((recipeIndex == 0), "The index was not reset to 0 !");
-        Recipe storage recipe = recipes[recipeIndex];
-        name = recipe.name;
-        ingredients = recipe.ingredients;
-        fundAmount = recipe.fundAmount;
-        timeForFund = recipe.timeForFund;
-        rewardIndexes = recipe.rewardIndexes;
+        Recipe memory recipe = recipes[recipeIndex];
+        return (
+            recipe.name,
+            recipe.ingredients,
+            recipe.fundAmount,
+            recipe.recipeOwnerAddress,
+            recipe.fundRaisedSoFar,
+            recipe.timeForFund
+        );
     }
 
-    // function getReward(
-    //     uint256 rewardIndex
-    // )
-    //     public
-    //     view
-    //     returns (uint256 fundAmount, string memory rewardDescription)
-    // {
-    //     require(rewardIndex < rewards.length, "Reward does not exist");
-    //     Reward storage reward = rewards[rewardIndex];
-    //     fundAmount = reward.fundAmount;
-    //     rewardDescription = reward.rewardDescription;
-    // }
-
     function sendFunding(uint256 indexOfRecipe) public payable {
-        require(msg.value > 0, "dont hack me bro");
+        require(msg.value > 0, "Please add more fund");
         recipes[indexOfRecipe].fundRaisedSoFar += msg.value;
     }
 
-    function getRecipeIndexFromOwnerAddressAndRecipeName(
-        string memory recipeName,
-        address recipeOwnerAddress
-    ) public view returns (int256) {
-        for (uint256 i = 0; i < recipes.length; i++) {
-            if (
-                recipes[i].recipeOwnerAddress == recipeOwnerAddress &&
-                keccak256(abi.encodePacked(recipes[i].name)) ==
-                keccak256(abi.encodePacked(recipeName))
-            ) {
-                return int256(i);
-            }
-        }
-        return -1;
-    }
+    function withdrawFunding(uint256 indexOfRecipe) public payable {
+        require(indexOfRecipe < recipes.length, "Recipe does not exist");
 
-    function withdrawFunding(string memory recipeName) public {
-        int256 recipeIndex = getRecipeIndexFromOwnerAddressAndRecipeName(
-            recipeName,
-            msg.sender
+        Recipe memory recipe = recipes[indexOfRecipe];
+
+        require(
+            msg.sender == recipe.recipeOwnerAddress,
+            "You are not the owner of this recipe"
         );
-        require(recipeIndex != -1, "Recipe not found or you are not the owner");
+        require(
+            recipe.fundRaisedSoFar > 0,
+            "No funds available for withdrawal"
+        );
 
-        address payable recipient = payable(msg.sender);
-        uint256 amountToWithdraw = recipes[uint256(recipeIndex)].fundAmount;
+        uint256 amountToWithdraw = recipe.fundRaisedSoFar;
+        recipe.fundRaisedSoFar = 0;
 
-        (bool sent, ) = recipient.call{value: amountToWithdraw}("");
-        require(sent, "Failed to send Ether");
+        payable(msg.sender).transfer(amountToWithdraw);
     }
 }
