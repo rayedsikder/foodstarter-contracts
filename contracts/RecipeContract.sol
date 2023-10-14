34434d818337029db4b3727c00548e1dc4f0c3d1
rayedsikder <rayedsikder33@gmail.com>
2023-10-14 16:56:39 +0600

    Add and incorporate recipeNameToIndex

--- contracts/RecipeContract.sol
+++ contracts/RecipeContract.sol
@@ -17,36 +17,37 @@ contract RecipeContract {
 
     Recipe[] public recipes;
     Reward[] public rewards;
+    mapping (string => uint256) public recipeNameToIndex;
 
-    function createReward(
-        uint256 _fundAmount,
-        string memory _rewardDescription
-    ) public {
-        Reward memory newReward;
-        newReward.fundAmount = _fundAmount;
-        newReward.rewardDescription = _rewardDescription;
-        rewards.push(newReward);
-    }
+    // function createReward(
+    //     uint256 _fundAmount,
+    //     string memory _rewardDescription
+    // ) public {
+    //     Reward memory newReward;
+    //     newReward.fundAmount = _fundAmount;
+    //     newReward.rewardDescription = _rewardDescription;
+    //     rewards.push(newReward);
+    // }
 
     function createRecipe(
         string memory _name,
         string[] memory _ingredients,
         uint256 _fundAmount,
-        uint256 _timeForFund,
-        uint256[] memory _rewardIndexes
+        uint256 _timeForFund
+        // uint256[] memory _rewardIndexes
     ) public {
-        require(_rewardIndexes.length > 0, "At least one reward is required");
-        require(
-            _rewardIndexes[_rewardIndexes.length - 1] < rewards.length,
-            "Invalid reward index"
-        );
-
+        // require(_rewardIndexes.length > 0, "At least one reward is required");
+        // require(
+        //     _rewardIndexes[_rewardIndexes.length - 1] < rewards.length,
+        //     "Invalid reward index"
+        // );
+        recipeNameToIndex[_name] = recipes.length;
         Recipe memory newRecipe;
         newRecipe.name = _name;
         newRecipe.ingredients = _ingredients;
         newRecipe.fundAmount = _fundAmount;
         newRecipe.timeForFund = _timeForFund;
-        newRecipe.rewardIndexes = _rewardIndexes;
+        // newRecipe.rewardIndexes = _rewardIndexes;
 
         recipes.push(newRecipe);
     }
@@ -73,16 +74,16 @@ contract RecipeContract {
         rewardIndexes = recipe.rewardIndexes;
     }
 
-    function getReward(
-        uint256 rewardIndex
-    )
-        public
-        view
-        returns (uint256 fundAmount, string memory rewardDescription)
-    {
-        require(rewardIndex < rewards.length, "Reward does not exist");
-        Reward storage reward = rewards[rewardIndex];
-        fundAmount = reward.fundAmount;
-        rewardDescription = reward.rewardDescription;
-    }
+    // function getReward(
+    //     uint256 rewardIndex
+    // )
+    //     public
+    //     view
+    //     returns (uint256 fundAmount, string memory rewardDescription)
+    // {
+    //     require(rewardIndex < rewards.length, "Reward does not exist");
+    //     Reward storage reward = rewards[rewardIndex];
+    //     fundAmount = reward.fundAmount;
+    //     rewardDescription = reward.rewardDescription;
+    // }
 }
